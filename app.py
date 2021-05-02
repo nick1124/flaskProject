34cb903cc173cc13216e7a7ca5206e9913cdca6d
nick <80083124+nick1124@users.noreply.github.com>
2021-05-02 13:51:51 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def header():
+    return '<h1 draggable="true">Flask App Example</h1>' \
+           '<h3 contenteditable="true">This text is editable. Write anything you want here.</h3>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
